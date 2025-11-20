d1922525da8f7ed5fab5a141ff6dc50c8d09bda0
huzaifalidev <huzaifalikhan48@gmail.com>
2025-11-20 16:56:47 +0500

    fixed the url

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 //const API_URL = process.env.NEXT_PUBLIC_DEV_URL;
-const API_URL = process.env.NEXT_PUBLIC_DEV_URL;
+const API_URL = process.env.NEXT_PUBLIC_PROD_URL;
 
 export const config = {
   API_URL,
